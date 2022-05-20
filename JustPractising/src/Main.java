91697daa6365b97b404a93edbaba74195a07cd3d
George <joricksteam@gmail.com>
2022-05-20 21:52:42 +0300

    main file changed

--- JustPractising/src/Main.java
+++ JustPractising/src/Main.java
@@ -3,23 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello NIGGAS");
-		
-		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
-		
-		
-		if (s.equals("George")) {
-			System.out.println("Hey, handsome");
-			
-		}	else if(s.equals("Sally")) {
-			System.out.println("Hey hoe");
-					}
-		else {
-			System.out.println("Who the fuck is you?");
-		}
-		in.close();
-			 				
+				
 		 
 		
 		
